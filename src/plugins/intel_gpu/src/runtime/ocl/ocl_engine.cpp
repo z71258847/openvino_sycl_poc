95d2c91b367b68683bb1f602417e34d1267cbe0b
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2022-01-26 14:35:26 +0300

    [GPU] Activation test works

--- src/plugins/intel_gpu/src/runtime/ocl/ocl_engine.cpp
+++ src/plugins/intel_gpu/src/runtime/ocl/ocl_engine.cpp
@@ -86,16 +86,16 @@ const cl::UsmHelper& ocl_engine::get_usm_helper() const {
 }
 
 memory::ptr ocl_engine::allocate_memory(const layout& layout, allocation_type type, bool reset) {
+    if (layout.is_dynamic()) {
+        throw std::runtime_error("[GPU] Can't allocate memory for dynamic layout");
+    }
+
     if (layout.bytes_count() > get_device_info().max_alloc_mem_size) {
         std::stringstream ss;
         ss << "Exceeded max size of memory object allocation: "
             << "Requested " << layout.bytes_count() << " bytes "
             << "but max alloc size is " << get_device_info().max_alloc_mem_size << " bytes";
         throw std::runtime_error(ss.str());
-    }
-
-    if (layout.is_dynamic()) {
-        throw std::runtime_error("[GPU] Can't allocate memory for dynamic layout");
     }
 
     if (type != allocation_type::cl_mem && !supports_allocation(type)) {
