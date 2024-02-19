f5492f0ee0c33eb3064e0a78c81dc6faf6a96b46
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-02-19 13:05:40 +0400

    Customize list of allowed data movement ops

--- src/common/transformations/include/transformations/common_optimizations/move_eltwise_up_data_movement.hpp
+++ src/common/transformations/include/transformations/common_optimizations/move_eltwise_up_data_movement.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "openvino/core/type.hpp"
 #include "openvino/pass/graph_rewrite.hpp"
 #include "transformations_visibility.hpp"
 
@@ -13,7 +14,10 @@ namespace pass {
 class TRANSFORMATIONS_API MoveEltwiseUpThroughDataMov : public ov::pass::MatcherPass {
 public:
     OPENVINO_RTTI("MoveEltwiseUpThroughDataMov", "0");
-    MoveEltwiseUpThroughDataMov();
+    MoveEltwiseUpThroughDataMov(std::vector<DiscreteTypeInfo> allowed_data_movement_ops = get_default_allowed_ops());
+
+private:
+    static std::vector<DiscreteTypeInfo> get_default_allowed_ops();
 };
 
 }  // namespace pass
