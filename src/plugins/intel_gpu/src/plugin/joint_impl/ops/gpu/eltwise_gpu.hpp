2d1a53d10d5179c662f47d3213913261b4132d97
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-02-23 10:46:32 +0400

    GPU eltwise stub

---
+++ src/plugins/intel_gpu/src/plugin/joint_impl/ops/gpu/eltwise_gpu.hpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "joint_impl/implementation_params.hpp"
+#include "joint_impl/op_implementation.hpp"
+
+namespace ov {
+namespace gpu {
+
+class SomeEltwiseGPUImpl : public OpImplementation {
+public:
+    SomeEltwiseGPUImpl() : OpImplementation("SomeEltwiseGPUImpl") {}
+
+    OpExecutor::Ptr get_executor(const ImplementationParameters* params) const override;
+    bool supports(const ImplementationParameters* params) const override;
+};
+
+}  // namespace gcpu
+}  // namespace ov
